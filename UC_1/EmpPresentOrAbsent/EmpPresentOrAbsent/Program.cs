24db9d667cce2a61b44c20184ee2eb4438031f91
faruqesay2 <faruqesay786@gmail.com>
2022-10-24 00:31:43 +0530

    Emp Is Absent Or Present

---
+++ UC_1/EmpPresentOrAbsent/EmpPresentOrAbsent/Program.cs
@@ -0,0 +1,18 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        int IS_FULL_TIME = 1;
+        Random random = new Random();
+        int empCheck = random.Next(0, 2);
+        if (empCheck == IS_FULL_TIME)
+{
+            Console.Writeline("Employee is Present");
+        }
+else
+        {
+            Console.Writeline("Employee is Present");
+        }
+    }
+}
