802340db67b204eb9129927397140079245c0542
faruqesay2 <faruqesay786@gmail.com>
2022-10-24 01:02:21 +0530

    Calculate Daily Emp Wage

---
+++ UC_2/CalculateDailyEmpWage/CalculateDailyEmpWage/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+public class Employee
+{
+    int ID;
+    string Name;
+    int Salary;
+    string Department;
+
+    static void Main(string[] args)
+    {
+        int TotalSalary = 0;
+
+        List<Employee> employees = new List<Employee>()
+        {
